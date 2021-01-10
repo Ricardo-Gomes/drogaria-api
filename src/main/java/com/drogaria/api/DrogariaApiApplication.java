4f7b01b615ec26afd847aaaa8ac0b2a6e4ec0a83
Ricardo-Gomes <rikardos38@gmail.com>
2021-01-10 18:37:41 -0300

    add ComponentScan

--- src/main/java/com/drogaria/api/DrogariaApiApplication.java
+++ src/main/java/com/drogaria/api/DrogariaApiApplication.java
@@ -2,8 +2,10 @@ package com.drogaria.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.drogaria.api.rest.controller"})
 public class DrogariaApiApplication {
 
 	public static void main(String[] args) {
