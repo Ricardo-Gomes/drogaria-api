936367ff63ce27a347db308dabdf3ce10e32996d
Ricardo-Gomes <rikardos38@gmail.com>
2021-01-10 18:43:02 -0300

    correção na classe application

--- src/main/java/com/drogaria/api/DrogariaApiApplication.java
+++ src/main/java/com/drogaria/api/DrogariaApiApplication.java
@@ -2,10 +2,8 @@ package com.drogaria.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.drogaria.api.rest.controller"})
 public class DrogariaApiApplication {
 
 	public static void main(String[] args) {
